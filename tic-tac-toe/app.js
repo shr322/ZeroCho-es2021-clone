248917547f953259d95614133d65fef3c16ffb78
심형래 <shr322@naver.com>
2022-08-01 23:07:01 +0900

    틱택토

---
+++ tic-tac-toe/app.js
@@ -0,0 +1,113 @@
+"use strict";
+
+let turn = "O";
+const data = [];
+
+for (let i = 0; i < 3; i++) {
+  data.push([]);
+}
+
+function checkWinner(target) {
+  let rowIndex;
+  let cellIndex;
+
+  rows.forEach((row, ri) => {
+    row.forEach((cell, ci) => {
+      if (cell === target) {
+        rowIndex = ri;
+        cellIndex = ci;
+      }
+    });
+  });
+
+  let hasWinner = false;
+
+  if (
+    rows[rowIndex][0].innerText === turn &&
+    rows[rowIndex][1].innerText === turn &&
+    rows[rowIndex][2].innerText === turn
+  ) {
+    hasWinner = true;
+  }
+
+  if (
+    rows[0][cellIndex].innerText === turn &&
+    rows[1][cellIndex].innerText === turn &&
+    rows[2][cellIndex].innerText === turn
+  ) {
+    hasWinner = true;
+  }
+
+  if (
+    rows[0][0].innerText === turn &&
+    rows[1][1].innerText === turn &&
+    rows[2][2].innerText === turn
+  ) {
+    hasWinner = true;
+  }
+  if (
+    rows[0][2].innerText === turn &&
+    rows[1][1].innerText === turn &&
+    rows[2][0].innerText === turn
+  ) {
+    hasWinner = true;
+  }
+
+  return hasWinner;
+}
+
+function callback(e) {
+  if (e.target.innerText !== "") {
+    console.log("빈칸이 아닙니다.");
+    return;
+  }
+  console.log("빈칸입니다.");
+  e.target.innerText = turn;
+
+  if (checkWinner(e.target)) {
+    $result.innerText = `${turn}님의 승리`;
+    $table.removeEventListener('click',callback);
+    return;
+  }
+
+
+  let draw = true;
+  rows.forEach((row)=>{
+    row.forEach((cell)=>{
+      console.log(!cell.innerText)
+      if(!cell.innerText){
+        draw = false;
+      }
+    })
+  })
+  
+  if(draw){
+    $result.innerText = '무승부';
+    return;
+  }
+
+
+
+  turn = turn === "O" ? "X" : "O";
+}
+
+const $table = document.createElement("table");
+const $result = document.createElement("div");
+const rows = [];
+for (let i = 0; i < 3; i++) {
+  const $tr = document.createElement("tr");
+  const cells = [];
+  for (let j = 0; j < 3; j++) {
+    const $td = document.createElement("td");
+    cells.push($td);
+
+    $tr.append($td);
+  }
+  rows.push(cells);
+
+  $table.append($tr);
+}
+$table.addEventListener("click", callback);
+
+document.body.append($table);
+document.body.append($result);
